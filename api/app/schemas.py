bf4a11529abd05868e42ca4a9e28e5eaa40967df
AA <37912140+aabayarea@users.noreply.github.com>
2023-06-28 08:56:17 -0700

    Update schemas.py

--- api/app/schemas.py
+++ api/app/schemas.py
@@ -1,14 +1,8 @@
-# Copyright (C) 2021-2023, Mindee.
-
-# This program is licensed under the Apache License 2.0.
-# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
-
 from typing import Tuple
 
 from pydantic import BaseModel, Field
 
 
-# Recognition output
 class RecognitionOut(BaseModel):
     value: str = Field(..., example="Hello")
 
@@ -18,4 +12,4 @@ class DetectionOut(BaseModel):
 
 
 class OCROut(RecognitionOut, DetectionOut):
-    pass
+    page_num: int  # Add this line
